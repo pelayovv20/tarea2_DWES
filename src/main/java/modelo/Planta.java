4b838befbeae2c383810031bef8bc981d56e2cef
pelayo <pelayovv20educastur.es>
2024-10-22 14:22:50 +0200

    añadido BD

---
+++ src/main/java/modelo/Planta.java
@@ -0,0 +1,50 @@
+package modelo;
+
+public class Planta {
+
+	private String codigo;
+	private String nombrecomun;
+	private String nombrecientifico;
+
+	public Planta() {
+
+	}
+
+	public Planta(String codigo, String nombrecomun, String nombrecientifico) {
+		super();
+		this.codigo = codigo;
+		this.nombrecomun = nombrecomun;
+		this.nombrecientifico = nombrecientifico;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombrecomun() {
+		return nombrecomun;
+	}
+
+	public void setNombrecomun(String nombrecomun) {
+		this.nombrecomun = nombrecomun;
+	}
+
+	public String getNombrecientifico() {
+		return nombrecientifico;
+	}
+
+	public void setNombrecientifico(String nombrecientifico) {
+		this.nombrecientifico = nombrecientifico;
+	}
+
+	@Override
+	public String toString() {
+		return "Planta [codigo=" + codigo + ", nombrecomun=" + nombrecomun + ", nombrecientifico=" + nombrecientifico
+				+ "]";
+	}
+
+}
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,21 +1,79 @@
 package principal;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import modelo.Planta;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("INI");
+		Scanner in = new Scanner(System.in);
 		
 		
+		System.out.println("Dame el código de la nueva Planta:");
+		String codigo = in.nextLine().trim().toUpperCase();
+		System.out.println("Dame el nombre común de la nueva planta:");
+		String nombre_comun = in.nextLine();
+		System.out.println("Dame el nombre científico de la nueva planta:");
+		String nombre_cientifico = in.nextLine();
 		
 		
+		Planta nueva = new Planta(codigo,nombre_comun,nombre_cientifico);
 		
+		Connection con;
+		MysqlDataSource m = new MysqlDataSource();
+		Properties prop = null;
+		FileInputStream fis;
+		String url = "jdbc:mysql://localhost:3306/basededatos";
+		String usuario = "root";
+		String password = "";
+		try {
+			fis = new FileInputStream("src/main/resources/db.properties");
+			prop.load(fis);
+			url = prop.getProperty("url");
+			usuario = prop.getProperty("usuario");
+			password = prop.getProperty("password");
+			m.setUrl(url);
+			m.setUser(usuario);
+			m.setPassword(password);
+			con = m.getConnection();
+			String sql = "INSERT INTO plantas(codigo,nombrecomun,nombrecientifico)"
+					+ "VALUES("+nueva.getCodigo()+" , " + nueva.getNombrecomun()+ " , " + nueva.getNombrecientifico();
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.execute();
+			
+			con.close();
+			
+			
+		}catch(SQLException e) {
+			System.out.println("error SQL" + e.getLocalizedMessage());
+			e.printStackTrace();
+		}catch(FileNotFoundException e) {
+			System.out.println("error");
+			e.printStackTrace();
+		}catch(IOException e ) {
+			System.out.println("error");
+			e.printStackTrace();
+			
+		}
 		
 		
 		
 		
 		
 		System.out.println("FIN");
-
 	}
 
 }
