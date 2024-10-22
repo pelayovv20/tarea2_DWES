83071de66e2a719d075f52765065514a55c24084
pelayo <pelayovv20educastur.es>
2024-10-22 12:51:44 +0200

    primer commit inicial

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,21 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("INI");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("FIN");
+
+	}
+
+}
